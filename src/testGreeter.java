458c3e549b4b3904de3b659260359e294b4b9e21
Unknown <20harmet@elmbrookstudents.org>
2018-02-16 12:02:10 -0600

    TestGreeterV2
    Array and for each loop

--- src/testGreeter.java
+++ src/testGreeter.java
@@ -3,5 +3,9 @@ public class testGreeter {
 	public static void main(String[] args) {
 		Greeter bob=new Greeter();
 		System.out.println(bob.toString());
+		Greeter[] array = {new Greeter(),new Greeter(), new Greeter()};
+		for(Greeter bib:array) {
+			System.out.println(bib.toString());
+		}
 	}
 }
